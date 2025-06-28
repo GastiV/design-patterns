ef83d49ba7bb89581481ecb120de88e773f8943d
Gastón Vidal <gnvidal25@gmail.com>
2025-06-28 20:51:54 -0300

    Builder pattern simple implementation

--- src/main/kotlin/design/patterns/designpatterns/DesignPatternsApplication.kt
+++ src/main/kotlin/design/patterns/designpatterns/DesignPatternsApplication.kt
@@ -1,28 +1,14 @@
 package design.patterns.designpatterns
 
 import design.patterns.designpatterns.builder.EmailAddress
-import design.patterns.designpatterns.builder.NotificationSettings
 import design.patterns.designpatterns.builder.PhoneNumber
-import design.patterns.designpatterns.builder.Subscription
-import design.patterns.designpatterns.builder.Subscription.*
-import design.patterns.designpatterns.builder.Subscription.Frequency.*
-import design.patterns.designpatterns.builder.Subscription.Topic.*
+import design.patterns.designpatterns.builder.createNotificationSettings
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DesignPatternsApplication
 
 fun main() {
-    createNotificationSettings()
-}
-
-fun createNotificationSettings(): NotificationSettings {
-    return NotificationSettings(
-        enabled = true,
-        subscriptions = listOf(
-            Subscription(EmailAddress("johndoe@example.com"), NEWS, WEEKLY),
-            Subscription(PhoneNumber("+1234567890"), ANALYTICS, DAILY)
-        )
-    )
+    createNotificationSettings(EmailAddress("johndoe@example.com"), null)
+    createNotificationSettings(EmailAddress("johndoe@example.com"), PhoneNumber("+1234567890"))
 }
--- src/main/kotlin/design/patterns/designpatterns/builder/BuilderPattern.kt
+++ src/main/kotlin/design/patterns/designpatterns/builder/BuilderPattern.kt
@@ -1,23 +1,41 @@
 package design.patterns.designpatterns.builder
 
-// Director class defines the order in which
-// to execute the different steps of the
-// building process.
-
+import design.patterns.designpatterns.builder.Subscription.Frequency
+import design.patterns.designpatterns.builder.Subscription.Frequency.*
+import design.patterns.designpatterns.builder.Subscription.Topic
+import design.patterns.designpatterns.builder.Subscription.Topic.*
+import kotlinx.collections.immutable.toImmutableList
 
 // Builder interface declares product
 // construction steps that are common to all
 // types of builders.
+
+interface INotificationSettingsBuilder{
+    var enabled: Boolean
+    fun addSubscription(dest: Destination, topic: Topic, freq: Frequency)
+    fun build(): NotificationSettings
+}
 
 // Concrete builders provide different implementations
 // of the construction steps. Concrete builders may
 // produce products that don't follow the common
 // interface.
 
+class NotificationSettingsBuilder: INotificationSettingsBuilder{
+    override var enabled: Boolean = false
+    private val subscriptions = mutableListOf<Subscription>()
+
+    override fun addSubscription(dest: Destination, topic: Topic, freq: Frequency) {
+        subscriptions.add(Subscription(dest, topic, freq))
+    }
+
+    override fun build(): NotificationSettings = NotificationSettings(enabled, subscriptions.toImmutableList())
+
+}
+
 // Products are resulting objects. Products constructed
 // by different builders don't have to belong to the
 // same class hierarchy or interface.
-
 
 data class NotificationSettings(val enabled: Boolean, val subscriptions: List<Subscription>)
 
@@ -32,5 +50,27 @@ sealed interface Destination
 value class EmailAddress(val value: String) : Destination
 @JvmInline
 value class PhoneNumber(val value: String) : Destination
+
+// Director class defines the order in which
+// to execute the different steps of the
+// building process.
+
+fun createNotificationSettings(email: EmailAddress?, phone: PhoneNumber?): NotificationSettings {
+
+    val builder: INotificationSettingsBuilder = NotificationSettingsBuilder()
+    if (email != null) { builder.addSubscription(email, ANALYTICS, DAILY) }
+    if (email != null) { builder.addSubscription(email, NEWS, WEEKLY) }
+    if (phone != null) { builder.addSubscription(phone, SECURITY_ALERTS, IMMEDIATELY) }
+    if (phone != null) { Subscription.Topic.entries.forEach{ builder.addSubscription(phone, it, IMMEDIATELY) } }
+
+    builder.enabled = true
+
+    val settings = builder.build()
+
+    println("Notification Settings: $settings")
+
+    return settings
+
+}
 
 
